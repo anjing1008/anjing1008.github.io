1c4d14aa0d6b06d9fffe31c3700f302528bbed33
runsisi <runsisi@hust.edu.cn>
2018-08-13 22:06:41 +0800

    update post page
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,32 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import "prismjs/themes/prism-okaidia.css";
 
-import asyncComponent from "../AsyncComponent";
+// import asyncComponent from "../AsyncComponent";
 import Headline from "../Article/Headline";
 import Bodytext from "../Article/Bodytext";
 import Meta from "./Meta";
-import Author from "./Author";
-import Comments from "./Comments";
+// import Author from "./Author";
+// import Comments from "./Comments";
 import NextPrev from "./NextPrev";
 
-const Share = asyncComponent(() =>
-  import("./Share")
-    .then(module => {
-      return module;
-    })
-    .catch(error => {})
-);
+// const Share = asyncComponent(() =>
+//   import("./Share")
+//     .then(module => {
+//       return module;
+//     })
+//     .catch(error => {})
+// );
 
 const Post = props => {
   const {
-    post,
+    // post,
     post: {
       html,
       fields: { prefix, slug },
       frontmatter: { title, author, category }
     },
-    authornote,
-    facebook,
+    // authornote,
+    // facebook,
     next: nextPost,
     prev: prevPost,
     theme
@@ -41,10 +41,10 @@ const Post = props => {
       </header>
       <Bodytext html={html} theme={theme} />
       <footer>
-        <Share post={post} theme={theme} />
-        <Author note={authornote} theme={theme} />
+        {/*<Share post={post} theme={theme} />*/}
+        {/*<Author note={authornote} theme={theme} />*/}
         <NextPrev next={nextPost} prev={prevPost} theme={theme} />
-        <Comments slug={slug} facebook={facebook} theme={theme} />
+        {/*<Comments slug={slug} facebook={facebook} theme={theme} />*/}
       </footer>
     </React.Fragment>
   );
@@ -52,8 +52,8 @@ const Post = props => {
 
 Post.propTypes = {
   post: PropTypes.object.isRequired,
-  authornote: PropTypes.string.isRequired,
-  facebook: PropTypes.object.isRequired,
+  // authornote: PropTypes.string.isRequired,
+  // facebook: PropTypes.object.isRequired,
   next: PropTypes.object,
   prev: PropTypes.object,
   theme: PropTypes.object.isRequired
