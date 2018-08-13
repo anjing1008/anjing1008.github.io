d70540b21192beaf0761fcff760ec102f9fb0165
runsisi <runsisi@hust.edu.cn>
2018-08-13 19:06:55 +0800

    remove the first sperator in list view
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- src/components/Blog/Item.js
+++ src/components/Blog/Item.js
@@ -88,7 +88,6 @@ const Item = props => {
 
           &:first-child {
             &::before {
-              border-top: 1px solid ${theme.line.color};
               content: "";
               height: 0;
               position: absolute;
