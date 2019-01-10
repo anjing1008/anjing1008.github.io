4153d4f7de1bb7ed55755791ce0aba99d78f1954
runsisi <runsisi@hust.edu.cn>
2019-01-10 10:07:25 +0800

    Updates

--- path---category-sayings-b931c398ebdabcbac2e2.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xf8217a05b0e6],{658:function(t,a){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{fields:{slug:"/draft-post/"},excerpt:"Proin suscipit luctus orci placerat fringilla. Donec hendrerit laoreet risus eget adipiscing. Suspendisse in urna ligula, a volutpat mauris…",timeToRead:1,frontmatter:{title:"This is a draft post",category:"sayings"}}}]},site:{siteMetadata:{facebook:{appId:""}}}},pathContext:{category:"sayings"}}}});
-//# sourceMappingURL=path---category-sayings-b931c398ebdabcbac2e2.js.map
--- path---category-yoga-152d67e2477b0aa0632c.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([69128309098937],{659:function(t,a){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{fields:{slug:"/hi-yoga/"},excerpt:"遇见瑜伽 唤醒身体的觉知力",timeToRead:1,frontmatter:{title:"Hi yoga",category:"yoga"}}}]},site:{siteMetadata:{facebook:{appId:""}}}},pathContext:{category:"yoga"}}}});
-//# sourceMappingURL=path---category-yoga-152d67e2477b0aa0632c.js.map
